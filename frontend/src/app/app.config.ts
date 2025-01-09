1a6b95836ddf2e78e08669abbce5183141a87714
Massimo Costa <costa.massimo@gmail.com>
2025-01-09 12:09:04 +0100

    feat: use zoneless change detection

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideExperimentalZonelessChangeDetection(),
     provideAnimationsAsync()
   ]
 };
